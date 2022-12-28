99de0719d5ac03b9d8214552a527c512196fd2a3
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2022-12-28 10:21:32 -0300

    feat: add gameStateFormatStream

--- workshop013-quake-challenge/transform-stream.mjs
+++ workshop013-quake-challenge/transform-stream.mjs
@@ -5,64 +5,55 @@ const gameState = {
   games: [],
 };
 
-const formatGamePlayers = ({ totalKills, players }, index) => {
-  const playersFormatted = Object.entries(players).reduce(
-    (acc, [id, value]) => {
-      const playerLabel = `${id}: ${value.totalKills}`;
+const initGame = () => {
+  gameState.currentGame += 1;
+  gameState.games.push({
+    totalKills: 0,
+    players: {},
+  });
+};
 
-      if (acc) return `${acc}, ${playerLabel}`;
-      return `${acc}${playerLabel}`;
-    },
-    ""
-  );
+const clientConnect = (actionData) => {
+  const [playerId] = actionData;
 
-  return `#GAME: ${index} | Players: ${playersFormatted} | Total Kills: ${totalKills}`;
+  gameState.games[gameState.currentGame].players[playerId] = {
+    totalKills: 0,
+  };
+};
+
+const clientUserinfoChanged = (actionData) => {
+  const [playerId, ...playerInfo] = actionData[0].split(" ");
+  const [, playerName] = playerInfo.join(" ").split("\\");
+
+  gameState.games[gameState.currentGame].players[playerId].name = playerName;
+};
+
+const kill = (actionData) => {
+  const [killerId] = actionData[0].split(" ");
+
+  if (gameState.games[gameState.currentGame].players[killerId]) {
+    gameState.games[gameState.currentGame].players[killerId].totalKills += 1;
+  }
+  gameState.games[gameState.currentGame].totalKills += 1;
 };
 
 const transformStream = new Transform({
+  objectMode: true,
   transform(chunk, encoding, callback) {
     const line = chunk.toString().trim();
     const [, ...parts] = line.split(" ");
     const [action, ...actionData] = parts.join(" ").split(": ");
 
-    if (action === "InitGame") {
-      gameState.currentGame += 1;
-      gameState.games.push({
-        totalKills: 0,
-        players: {},
-      });
-    }
+    const actions = {
+      InitGame: initGame,
+      ClientConnect: () => clientConnect(actionData),
+      ClientUserinfoChanged: () => clientUserinfoChanged(actionData),
+      Kill: () => kill(actionData),
+    };
 
-    if (action === "ClientConnect") {
-      const [playerId] = actionData;
+    if (action in actions) actions[action]();
 
-      gameState.games[gameState.currentGame].players[playerId] = {
-        totalKills: 0,
-      };
-    }
-
-    if (action === "Kill") {
-      const [killerId] = actionData[0].split(" ");
-
-      if (gameState.games[gameState.currentGame].players[killerId]) {
-        gameState.games[gameState.currentGame].players[
-          killerId
-        ].totalKills += 1;
-      }
-      gameState.games[gameState.currentGame].totalKills += 1;
-    }
-
-    if (action === "Shutdown") {
-      callback(null, `\nGAME OVER\n`);
-      return;
-    }
-
-    const totalKillsPerGame = gameState.games
-      .map(formatGamePlayers)
-      .join("\n")
-      .concat("\n");
-
-    callback(null, "\u001Bc" + totalKillsPerGame);
+    callback(null, gameState);
   },
 });
 
