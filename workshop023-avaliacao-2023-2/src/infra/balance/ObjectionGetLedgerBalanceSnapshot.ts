9d7c1ec47facaf46f8f6dfd2c7bc05240d1367dd
Paulo Renato <paulo.souza@codeminer42.com>
2023-09-06 08:40:52 -0300

    Implement the service to get a ledger's balance snapshot

--- workshop023-avaliacao-2023-2/src/infra/balance/ObjectionGetLedgerBalanceSnapshot.ts
+++ workshop023-avaliacao-2023-2/src/infra/balance/ObjectionGetLedgerBalanceSnapshot.ts
@@ -1,7 +1,31 @@
 import { Ledger } from '../../domain/ledger/Ledger';
+import { Balance } from '../../queries/BalanceQuery';
+import { BalanceSnapshotEntryModel } from '../database/models/BalanceSnapshotEntryModel';
+import { BalanceSnapshotModel } from '../database/models/BalanceSnapshotModel';
 import { BalanceSnapshot } from './BalanceSnapshot';
 
-export const makeObjectionGetLedgerBalanceSnapshot = (): GetLedgerBalanceSnapshot => () => Promise.resolve(null);
+type SnapshotWithEntriesQueryResult = Array<
+  Pick<BalanceSnapshotModel, 'created_at'> & Pick<BalanceSnapshotEntryModel, 'amount' | 'currency'>
+>;
+
+export const makeObjectionGetLedgerBalanceSnapshot = (): GetLedgerBalanceSnapshot => async (ledgerId) => {
+  const snapshotWithEntries = (await BalanceSnapshotModel.query()
+    .select(['created_at', 'amount', 'currency'])
+    .joinRelated('balance_snapshot_entries')
+    .where('ledger_id', ledgerId ?? null)) as unknown as SnapshotWithEntriesQueryResult;
+
+  if (snapshotWithEntries.length === 0) return null;
+
+  const balance = snapshotWithEntries.reduce(
+    (queryResult, currencyBalance) => ({
+      ...queryResult,
+      [currencyBalance.currency]: Number(currencyBalance.amount),
+    }),
+    {} as Balance
+  );
+
+  return { createdAt: snapshotWithEntries[0].created_at, balance };
+};
 
 export type GetLedgerBalanceSnapshot = (
   ledgerId?: Ledger.Type['id']
