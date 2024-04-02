9318776a66b6fc23867996ee7696ca89e56ba7ed
Paulo Souza <paulo.souza@codeminer42.com>
2024-04-02 18:27:50 -0300

    Create Torrent and TorrentFile entities and create the initial db migration

---
+++ workshop025-torrent-client/src/torrents/infrastructure/TorrentEntity.ts
@@ -0,0 +1,66 @@
+import {
+  BaseEntity,
+  Collection,
+  Entity,
+  EntityDTO,
+  Enum,
+  ManyToOne,
+  OneToMany,
+  PrimaryKey,
+  Property,
+  Reference,
+} from "@mikro-orm/core";
+import { TorrentStatus } from "../domain/Torrent.js";
+
+@Entity({ tableName: "torrents" })
+export class TorrentEntity extends BaseEntity {
+  @PrimaryKey({ type: "varchar", length: 40 })
+  id: string;
+
+  @Property({ type: "varchar", length: 256 })
+  name: string;
+
+  @Enum({ items: Object.values(TorrentStatus) })
+  status: TorrentStatus;
+
+  @Property({ type: "datetime" })
+  startedAt: Date;
+
+  @OneToMany({
+    entity: "TorrentFileEntity",
+    mappedBy: "torrent",
+    orphanRemoval: true,
+  })
+  files: Collection<TorrentFileEntity>;
+
+  constructor(props: EntityDTO<TorrentEntity>) {
+    super();
+    this.id = props.id;
+    this.name = props.name;
+    this.status = props.status;
+    this.startedAt = props.startedAt;
+    this.files = new Collection(
+      this,
+      props.files.map((file) => new TorrentFileEntity(file))
+    );
+  }
+}
+
+@Entity({ tableName: "torrent_files" })
+export class TorrentFileEntity extends BaseEntity {
+  @PrimaryKey({ type: "uuid" })
+  id: string;
+
+  @Property({ type: "varchar", length: 256 })
+  name: string;
+
+  @ManyToOne({ entity: "TorrentEntity", inversedBy: "files" })
+  torrent: Reference<TorrentEntity>;
+
+  constructor(props: EntityDTO<TorrentFileEntity>) {
+    super();
+    this.id = props.id;
+    this.name = props.name;
+    this.torrent = Reference.createFromPK(TorrentEntity, props.torrent);
+  }
+}
